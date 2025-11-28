cf0e05825af53a5ee68b865add3a1095feb399d6
Noel De Martin <noel@noeldemartin.com>
2025-11-28 17:57:53 +0100

    Fix auth

--- src/auth.js
+++ src/auth.js
@@ -8,11 +8,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const auth = betterAuth({
-    plugins: [jazzPlugin()],
-    trustedOrigins: [process.env.TRUSTED_ORIGINS.split(",")],
-    database: new Database(join(__dirname, "../db/sqlite.db")),
-    emailAndPassword: {
-        enabled: true,
-        disableSignUp: process.env.NODE_ENV === "production",
-    },
-})
+  plugins: [jazzPlugin()],
+  trustedOrigins: process.env.TRUSTED_ORIGINS.split(","),
+  database: new Database(join(__dirname, "../db/sqlite.db")),
+  emailAndPassword: {
+    enabled: true,
+    disableSignUp: process.env.NODE_ENV === "production",
+  },
+});
